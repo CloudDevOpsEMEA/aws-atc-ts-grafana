00a593925a93c075e8c6a309aad021471f54fb37
Bart Van Bos <bartvanbos@gmail.com>
2020-04-24 02:38:32 +0200

    add support to also run terraform and ansible directly in the corresponding folders without the makefile

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "setupfile" {
   description = "The setup file in yaml format"
   type        = string
-  default     = "setup.yml"
+  default     = "../setup.yml"
 }
 
 variable "ec2privatekey" {
   description = "The AWS EC2 private key"
   type        = string
-  default     = "ec2_private_key.pem"
+  default     = "../output/ec2_private_key.pem"
 }
 
 variable "awsinventoryconfig" {
   description = "The configuration file for AWS dynamic inventory in Ansible"
   type        = string
-  default     = "aws_ec2.yml"
+  default     = "../output/aws_ec2.yml"
 }
 
 variable "generateloadscript" {
   description = "The bash script to generate HTTP/HTTPS load towards the BIG-IP VIP"
   type        = string
-  default     = "generate_load.sh"
+  default     = "../output/generate_load.sh"
 }
